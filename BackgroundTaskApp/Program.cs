8bff460862b306c5d42960b8fd96aec8e2faaedc
farooq-teqniqly <farooq@teqniqly.com>
2024-06-27 15:37:35 -0700

    ASP.NET Core background service example

--- BackgroundTaskApp/Program.cs
+++ BackgroundTaskApp/Program.cs
@@ -6,11 +6,6 @@ namespace BackgroundTaskApp
         {
             var builder = WebApplication.CreateBuilder(args);
 
-            builder.Services.Configure<HostOptions>(options =>
-            {
-                options.BackgroundServiceExceptionBehavior = BackgroundServiceExceptionBehavior.StopHost;
-            });
-
             builder.Services.AddHostedService<ExampleBackgroundService>();
 
             var app = builder.Build();
@@ -30,29 +25,10 @@ namespace BackgroundTaskApp
 
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
         {
-            try
+            while (!stoppingToken.IsCancellationRequested)
             {
-                var counter = 0;
-
-                while (!stoppingToken.IsCancellationRequested)
-                {
-                    if (counter == 10)
-                    {
-                        await StopAsync(stoppingToken);
-                    }
-
-                    _logger.LogInformation("Hello");
-                    counter++;
-                    await Task.Delay(1000, stoppingToken);
-                }
-            }
-            catch (TaskCanceledException)
-            {
-                _logger.LogInformation("Cancellation requested.");
-            }
-            finally
-            {
-                _logger.LogInformation("Bye");
+                _logger.LogInformation("Hello");
+                await Task.Delay(1000, stoppingToken);
             }
         }
     }
